21c961173c7747d1a5992eea7ebe202ebfd1036e
dipayan debnath <debnathdipayan042@gmail.com>
2024-05-14 13:29:49 +0530

    first commit

---
+++ mint.ts
@@ -0,0 +1,36 @@
+import { percentAmount, generateSigner, signerIdentity, createSignerFromKeypair } from '@metaplex-foundation/umi'
+import { TokenStandard, createAndMint } from '@metaplex-foundation/mpl-token-metadata'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import  "@solana/web3.js";
+import secret from './guideSecret.json';
+
+const umi = createUmi('https://burned-withered-arrow.solana-devnet.quiknode.pro/5f0a71345dc90eb606c79642db8ed87fc4805178/'); //Replace with your QuickNode RPC Endpoint
+
+const userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secret));
+const userWalletSigner = createSignerFromKeypair(umi, userWallet);
+
+const metadata = {
+    name: "Cat With Hat",
+    symbol: "CWH",
+    uri: "https://crimson-rare-turkey-790.mypinata.cloud/ipfs/QmUSvZqW4119YkyfCuEWZCFtjN1GbUXyJyPzAFRe9CX4ov",
+};
+
+const mint = generateSigner(umi);
+umi.use(signerIdentity(userWalletSigner));
+umi.use(mplCandyMachine())
+
+createAndMint(umi, {
+    mint,
+    authority: umi.identity,
+    name: metadata.name,
+    symbol: metadata.symbol,
+    uri: metadata.uri,
+    sellerFeeBasisPoints: percentAmount(0),
+    decimals: 8,
+    amount: 1000000_00000000,
+    tokenOwner: userWallet.publicKey,
+    tokenStandard: TokenStandard.Fungible,
+    }).sendAndConfirm(umi).then(() => {
+    console.log("Successfully minted 1 million tokens (", mint.publicKey, ")");
+});
